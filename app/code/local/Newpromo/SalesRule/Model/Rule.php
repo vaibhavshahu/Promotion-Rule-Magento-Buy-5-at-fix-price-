b0e29719e8b173b1b8eeecc48763e5ad0fbe6880
Vaibhav Shahu <vaibhavshahu@gmail.com>
2015-08-03 12:45:43 +0530

    Create Rule.php

---
+++ app/code/local/Newpromo/SalesRule/Model/Rule.php
@@ -0,0 +1,5 @@
+//Add new rule by overriding Mage_SalesRule_Model_Rule
+class Newpromo_SalesRule_Model_Rule extends Mage_SalesRule_Model_Rule
+{    
+    const BUY_5_AT_FIX_PRICE_ACTION = 'buy_5_at_fix_price';
+}
